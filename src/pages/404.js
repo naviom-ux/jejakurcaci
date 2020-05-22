c554850323e532b66e688ba2b7fd607e9d2b46f9
Willy Ahmad Syafiq <willy.syafiq@Willys-MBP.local>
2020-05-22 07:45:47 +0700

    new 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,39 @@
 import React from 'react'
-import LayoutHome from '../components/layout-home'
+import '../components/style.scss';
+import { graphql, useStaticQuery, Link } from 'gatsby';
+import Helmet from '../components/helmet';
+import SideMenu from '../components/side-menu';
+import Footer from '../components/footer';
+import NavbarHome from '../components/navbar-home';
+import Image1 from '../images/slides/nakul&adit/jejakurcaci_nakul_adit_couplesession-39.jpg';
+import ArrowLBlack from '../images/arrow-left-black.svg';
 
 const NotFoundPage = () => (
-  <LayoutHome>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </LayoutHome>
+
+  <div className="columns is-gapless">
+      <div className="column side-menu">
+          <SideMenu/>
+      </div>	
+      <div className="column is-rest">	
+
+          <Helmet />
+
+          <nav className="is-hidden-mobile navbar-general">
+              <NavbarHome/>
+          </nav>
+
+          <div className="columns is-multiline is-centered no-bot-margin not-found">
+              <div className="column is-10">
+                <h1>404</h1>
+                <h2>The page you requested could not be found</h2>
+                <p><Link to="/" className="back-button">Back to Homepage</Link></p>
+              </div>  
+          </div>
+
+          <Footer/>
+      </div>
+  </div>
+  
 )
 
 export default NotFoundPage
